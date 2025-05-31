f560ab83f825beec4c5d539dbf01e75b355fef5b
ruraeru <f93335383@gmail.com>
2025-05-31 14:30:21 +0900

    .env error fix

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,22 +1,16 @@
 async function getPosts() {
     // await new Promise((res) => setTimeout(res, 5000));
     try {
-        const response = await fetch(`${process.env.API_HOST}/posts`);
-        const data2 = await test.json();
-        console.log(data2)
+        const response = await fetch(`${process.env.REACT_APP_API_HOST}/posts`);
 
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
 
         const data = await response.json();
-        console.log(data);
         return data;
     } catch (e) {
         console.error(e.message);
     }
 }
 export { getPosts };
-
-
-
